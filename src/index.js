bbd5e4f26d1520c2a3cb58eea44395186555580f
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-25 15:46:39 -0800

    Dynamically making image list!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+import images from '../data/images.js';
+import makeImageTemplate from './make-image-template.js';
+
+const imageList = document.getElementById('image-list');
+
+images.forEach(image => {
+    const dom = makeImageTemplate(image);
+    imageList.appendChild(dom);
+});
