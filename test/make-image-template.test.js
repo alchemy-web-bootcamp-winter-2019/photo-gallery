c5d6046ca6de868cdcbd25151acd5400d4def595
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-25 15:27:13 -0800

    First failing test

---
+++ test/make-image-template.test.js
@@ -0,0 +1,17 @@
+const test = QUnit.test;
+
+QUnit.module('Creating image template');
+
+test('return dom version of HTML', assert => {
+    // arrange
+    const expected = `
+        <li>
+            <h3>UniWhal</h3>
+            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+        </li>
+    `;
+    // act
+    const result = makeImageTemplate();
+    // assert
+    assert.htmlEqual(result, expected);
+});
--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,2 @@
 import './html-equal.js';
+import './make-image-template.test.js';
