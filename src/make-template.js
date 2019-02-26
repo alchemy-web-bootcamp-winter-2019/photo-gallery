706e5076358b73e80b5ecad79fd088cf676d93e1
ollinieminen <otn987@gmail.com>
2019-02-25 17:54:18 -0800

    commit where currently at with project

--- src/make-template.js
+++ src/make-template.js
@@ -1,1 +1,13 @@
 // template function and export it
+
+
+function makeHtmlTemplate (item) {
+const template = document.createElement('template');
+template.innerHTML=`<li>
+ <h2>${item.title}</h2>
+ <img sr
+</li>`
+
+   
+
+;}
--- test/tests.js
+++ test/tests.js
@@ -1,16 +1,30 @@
+import './html-equal.js';
+
 const test = QUnit.test;
 
 
-test('make html from template with interpolation',function(assert) {
+test('make html from template with interpolation', function(assert) {
 
     //arrange
-    const dom = 'hi';
+    const dom =  
+    `<ul> 
+         <li>
+            <h2>UniWhal</h2>
+            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"></img>
+        </li>
+    </ul>`;
 
-    const expected = 'hi'; 
+    const expected = 
+    `<ul>
+         <li>
+            <h2>UniWhal</h2>
+            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"></img>
+        </li>
+    </ul>`;
     //act
 
 
 
     //assert
-    assert.equal(dom, expected);
+    assert.htmlEqual(dom, expected);
 });
