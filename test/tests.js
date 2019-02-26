de4f21104716d335da14f49003817663449f6b91
ollinieminen <otn987@gmail.com>
2019-02-25 21:16:35 -0800

    got test working

--- test/tests.js
+++ test/tests.js
@@ -11,16 +11,16 @@ test('make html from template with interpolation', function(assert) {
         title: 'UniWhal',
         url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
     };
-    const expected = '<li><h2>Horned Animal Galery</h2><img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"></li>';
+    const expected = '<li><h2>UniWhal</h2><img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"></li>';
     //act
     function createHtmlTemplate(imageObject) {
         const html = `<li><h2>${imageObject.title}</h2><img src="${imageObject.url}"></li>`;
         const template = document.createElement('template');
         template.innerHTML = html;
-        return template;
+        return template.content;
     }
     const dom = createHtmlTemplate(imageObject);
-    console.log(dom);
+  
 
     //assert
     assert.htmlEqual(dom, expected);
