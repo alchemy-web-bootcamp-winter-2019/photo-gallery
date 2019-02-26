004190831b1fbbbb5be768b090b809d696fcdc81
heather <unphurlng@gmail.com>
2019-02-25 17:35:13 -0800

    test passing

--- test/file1.test.js
+++ test/file1.test.js
@@ -1,7 +1,34 @@
 const test = QUnit.test;
 
-QUnit.module('test suite one');
+QUnit.module('Create URL Template');
 
-test('time to write a test', function(assert) {
-    assert.equal(true, false);
+const image = {
+    title: 'UniWhal',
+    url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
+};
+
+function createHtmlTemplate() {
+    return `
+        <li>
+            <p>UniWhal</p>
+            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+        </li>
+    `;
+}
+
+test('Display title & image', function(assert) {
+    // arrange
+    const expected = `
+        <li>
+            <p>UniWhal</p>
+            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+        </li>
+    `;
+
+    // act
+    const result = createHtmlTemplate(image);
+
+
+    // assert
+    assert.equal(result, expected);
 });
