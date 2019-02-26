0c4872871a1bb128f90798619d5a43fa3bb84240
Peter Kotowski <bighoopee@gmail.com>
2019-02-25 21:33:13 -0800

    restored to original test files to pass travis

--- tests/tests.js
+++ tests/tests.js
@@ -1,9 +1,4 @@
 import './html-equal.js';
 import './file1.test.js';
-// import './file2.test.js';
+import './file2.test.js';
 
-const test = QUnit.test;
-
-test('time to write a test', function(assert) {
-    assert.equal(true, false);
-});
