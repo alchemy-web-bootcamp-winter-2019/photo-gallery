23a92815ff552f90004a8aebac53f41869162a22
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-02-25 16:16:19 -0800

    set up QUnit test, test is failing due to no function

--- tests/tests.js
+++ tests/tests.js
@@ -4,9 +4,17 @@ QUnit.module('testing gallery outputs');
 
 test('checking if template function outputs html correctly', function(assert) {
     // arrange
-
+    const image = {
+        title: 'UniWhal',
+        url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
+    }
+    const expected = `        
+    <li>
+        <h2>UniWhal</h2>
+        <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="image">
+    </li>`;
     // act
-
+    const results = makeHtmlTemplate(image);
     // assert
-
+    assert.htmlEqual(result, expected);
 });
