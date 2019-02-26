48c004eb48ad6aeed09ef034ae2cc3c8b8de2cf0
Peter Kotowski <bighoopee@gmail.com>
2019-02-25 22:35:37 -0800

    test fails, started over

--- src/make-gallery-template.js
+++ src/make-gallery-template.js
@@ -1,15 +1,1 @@
-function makeGalleryFunction(image) {
-    const html = /*html*/`
-    <li>
-        <h2>${image.title}</h2>
-        <img src="${image.url}">
-    </li>`;
-    
-    const template = document.createElement('li');
-    template.innerHTML = html;
-    return template.content;
 
-
-}
-
-export default makeGalleryFunction();
--- test/file1.test.js
+++ test/file1.test.js
@@ -1,21 +1,34 @@
-import makeGalleryFunction from '../src/make-gallery-template.js';
 
 const test = QUnit.test;
 
 QUnit.module('test suite one');
 
+const image = {
+    'url': 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+    'title': 'UniWhal'
 
-test('make a template that does something', function(assert) {
-    const result = makeGalleryFunction ({
-        url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
-        title: 'UniWhal'
-    });
+};
 
 
-    assert.htmlEqual(result, /*html*/`
-        <li>
-            <h2>Uniwhal</h2>
-            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
-        </li>
-    `);
+function makeGalleryFunction(image) {
+    return image;
+
+
+}
+
+test('create a template function', function(assert) {
+    const expected = {
+        'url': 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+        'title': 'UniWhal'
+    
+    };
+    
+    const result = makeGalleryFunction(image);
+    
+    assert.equal(expected, result);
 });
+
+
+
+
+
