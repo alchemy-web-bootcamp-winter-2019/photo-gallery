ecaaae3b151a5d3f4a4687e3cf533af397d099b4
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-02-25 16:09:59 -0800

    file structure set up, html hard coded to show three image containers

---
+++ tests/tests.js
@@ -0,0 +1,12 @@
+const test = QUnit.test;
+
+QUnit.module('testing gallery outputs');
+
+test('checking if template function outputs html correctly', function(assert) {
+    // arrange
+
+    // act
+
+    // assert
+
+});
