f91615eb0ffb4f459cdd075eed9309f4a2c9ee12
Peter Kotowski <bighoopee@gmail.com>
2019-02-25 20:31:48 -0800

    un-commented file2.js to pass travis

--- tests/file2.test.js
+++ tests/file2.test.js
@@ -1,7 +1,7 @@
-// const test = QUnit.test;
+const test = QUnit.test;
 
-// QUnit.module('test suite two');
+QUnit.module('test suite two');
 
-// test('time to write a test', function(assert) {
-//     assert.equal(true, false);
-// });
+test('time to write a test', function(assert) {
+    assert.equal(true, false);
+});
