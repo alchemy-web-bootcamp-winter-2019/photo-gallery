575a8e1784035af79a719fd306bfae9f431b363a
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-02-25 16:29:11 -0800

    first hard coded test is working

--- tests/tests.js
+++ tests/tests.js
@@ -1,20 +1,33 @@
 const test = QUnit.test;
 
+import './html-equal.js';
+
 QUnit.module('testing gallery outputs');
 
-test('checking if template function outputs html correctly', function(assert) {
+function makeHtmlTemplate(image) {
+    const template = document.createElement('template');
+    template.innerHTML = `        
+    <li>
+        <h2>UniWhal</h2>
+        <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="image">
+    </li>`;
+
+    return template.content;
+}
+
+test('checking if template function outputs html correctly', assert => {
     // arrange
     const image = {
         title: 'UniWhal',
         url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
-    }
+    };
     const expected = `        
     <li>
         <h2>UniWhal</h2>
         <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="image">
     </li>`;
     // act
-    const results = makeHtmlTemplate(image);
+    const result = makeHtmlTemplate(image);
     // assert
     assert.htmlEqual(result, expected);
 });
