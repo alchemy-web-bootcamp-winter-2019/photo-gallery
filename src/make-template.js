095e96fcc87bd33bfa9322b86c1a40abb44b1dad
ollinieminen <otn987@gmail.com>
2019-02-25 16:55:54 -0800

    first commit for reboot

---
+++ src/make-template.js
@@ -0,0 +1,1 @@
+// template function and export it
---
+++ test/tests.js
@@ -0,0 +1,16 @@
+const test = QUnit.test;
+
+
+test('make html from template with interpolation',function(assert) {
+
+    //arrange
+    const dom = 'hi';
+
+    const expected = 'hi'; 
+    //act
+
+
+
+    //assert
+    assert.equal(dom, expected);
+});
