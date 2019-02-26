07284e693feb91f39604550912993aa8a8cbb5a5
Peter Kotowski <bighoopee@gmail.com>
2019-02-25 19:24:46 -0800

    having issues with tdd, may need to check commit histories

--- tests/file1.test.js
+++ tests/file1.test.js
@@ -1,7 +1,21 @@
+import makeGalleryFunction from '../src/make-gallery-template.js';
+
 const test = QUnit.test;
 
 QUnit.module('test suite one');
 
-test('time to write a test', function(assert) {
-    assert.equal(true, false);
+
+test('make a template that does something', function(assert) {
+    const result = makeGalleryFunction ({
+        url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+        title: 'UniWhal'
+    });
+
+
+    assert.htmlEqual(result, /*html*/`
+        <li>
+            <h2>Uniwhal</h2>
+            <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg">
+        </li>
+    `);
 });
