1a686749b746491a562965bba0b4609c1d2ef933
Cosmo Kent <cosmolightfoot@gmail.com>
2019-02-25 17:03:16 -0800

    hardcoded html images, began testing

--- test/file1.test.js
+++ test/file1.test.js
@@ -1,7 +1,13 @@
 const test = QUnit.test;
 
-QUnit.module('test suite two');
+QUnit.module('Template Literal Function');
 
-test('time to write a test', function(assert) {
-    assert.equal(true, false);
+test('Write a function that returns a specific html code dynamically', function(assert) {
+    
+    
+    const expected = `
+    <li><h2>UniWhal</h2>
+    <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="A unicorn and a narwhal nuzzling their horns">
+    </li>`
+    assert.equal(result, expected);
 });
